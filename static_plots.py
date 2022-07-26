80339bf50115483c041a7baf5a64996ff52c0017
boongying <boongying@gmail.com>
2022-07-26 18:53:21 +0900

    updated static_plots.py

--- static_plots.py
+++ static_plots.py
@@ -8,7 +8,8 @@ stageIndices = [0,0,0,1,1,1,0,0,0,1,1,1,1,0,1,0]
 stageNames = ['North-South','West-East']
 bar_colours = ['c','m']
 
-tlsdf = pd.read_xml('TLSrecord.xml')
+tlsdf = pd.read_xml('TLSrecord1.xml')
+
 #just to change column name 'phase' to 'subStageID'
 colnames = tlsdf.columns.to_series()
 colnames.loc[colnames == 'phase'] = 'subStageID'
@@ -25,8 +26,7 @@ There are 2 variations of cyclicity plot
 2) A vertical bar is stacked until all stages are in the bar.
 '''
 
-fig, axes = plt.subplots(figsize = (10,7), ncols = 1, nrows = max(stageIndices) + 1)
-
+fig, axes = plt.subplots(figsize = (10,10), ncols = 1, nrows = max(stageIndices) + 2)
 
 greenSubStages = {}
 for column in  stages:
@@ -35,7 +35,7 @@ flattenedGreen = [i for j in list(greenSubStages.values()) for i in j]
 bar_colours = pd.Series(bar_colours, index = flattenedGreen) # watch out, this may not work when a stage is green at more than 1 subStages
 bar_names = pd.Series(stageNames, index = flattenedGreen) # watch out, this may not work when a stage is green at more than 1 subStages
 
-for st_stage, ax in enumerate(axes):
+for st_stage, ax in enumerate(axes[:-1]):
     tlsnp_cut = tlsnp[tlsnp[:,1].tolist().index(flattenedGreen[st_stage]):]
     bar_loc = tlsnp_cut[0,0]
     storage = []
@@ -69,9 +69,9 @@ for st_stage, ax in enumerate(axes):
     ax.set_ylabel('Cycle split (-)')
     ax.xaxis.grid(True)
 
-    ax.set_xlim(0,600)
+    ax.set_xlim(0,5000)
     ax.set_ylim(0,1)
-    ax.set_aspect(200, adjustable='box')
+    ax.set_aspect(1000, adjustable='box')
 
 plt.sca(axes[0])
 handles, labels = plt.gca().get_legend_handles_labels()
@@ -79,4 +79,19 @@ by_label = dict(zip(labels, handles))
 plt.legend(by_label.values(), by_label.keys(), 
             bbox_to_anchor = (1.0,1.05), loc = 'lower right',
             borderaxespad = 0)
+
+# plot for state history
+s_rl = pd.read_csv('s.csv', index_col = 0)
+s_rl['time'] = np.arange(0,s_rl.shape[0]*10-1,10)
+s_rl['West-East'] = s_rl[['0','2']].max(axis = 1)
+s_rl['North-South'] = s_rl[['1','3']].max(axis = 1)
+
+axes[-1].step(s_rl['time'],s_rl['North-South'], linewidth = 0.9,color = 'c')
+axes[-1].step(s_rl['time'],s_rl['West-East'], linewidth = 0.9, color = 'm')
+
+axes[-1].set_xlabel('Simulation time (s)')
+axes[-1].set_ylabel('Critical queuing number (veh)')
+axes[-1].xaxis.grid(True)
+axes[-1].set_xlim(0,5000)
+
 plt.show()
